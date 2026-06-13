bd7d9165d11688bb6bcb043f0cdd23a7960eb838
Sakib Shaikh <147747620+sakib92s@users.noreply.github.com>
2026-06-13 11:57:18 +0530

    Add customer_footer.php with footer HTML structure

---
+++ finvault/customer/includes/customer_footer.php
@@ -0,0 +1,7 @@
+    </main>
+  </div><!-- /.main -->
+</div><!-- /.app-shell -->
+<div id="toastHost" class="toast-host"></div>
+<script src="<?= BASE_URL ?>/assets/js/main.js"></script>
+</body>
+</html>
